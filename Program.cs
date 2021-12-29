2b51edfb9560e9a7dfe43efb32007f9c3ecd2034
Cleber Shimizu <cleber.shimizu@gmail.com>
2021-12-29 20:31:07 -0300

    Added DTO and Mapping. Core data structure completed, typos fixed

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
